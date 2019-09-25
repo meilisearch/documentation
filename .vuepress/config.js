44c9e2e36084e34df3a20d1b4dc7dab6c270a17b
Clément Renault <clement@meilisearch.com>
2019-09-25 13:10:41 +0200

    Add the Crisp chatbox at bottom right

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,19 @@
 module.exports = {
   title: 'Meili Documentation',
   description: 'The Meili incredible documentation',
+  head: [
+    ['script', {}, `
+      window.$crisp=[];
+      window.CRISP_WEBSITE_ID="38a71dd2-729c-4970-b061-3e1db1c1eb20";
+      (function() {
+          var d=document;
+          var s=d.createElement("script");
+          s.src="https://client.crisp.chat/l.js";
+          s.async=1;
+          d.getElementsByTagName("head")[0].appendChild(s);
+      })();
+    `]
+  ],
   themeConfig: {
     sidebar: [
       '/',
