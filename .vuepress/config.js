7e081e8a0ffe327b710a88c335f8ffc09b6e3545
Clementine Urquizar <clementine@meilisearch.com>
2020-02-28 21:20:06 +0100

    Integrate MeiliSearch

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -108,6 +108,12 @@ module.exports = {
         }
       ]
     },
+    meilisearch: {
+      hostUrl: 'https://e10b17e6.getmeili.com',
+      indexUid: '9tz3lqoi',
+      apiKey: 'R62XWZPJG794YB5VFOADHTNQEKLCISM01U38',
+    },
+    searchPlaceholder: 'Search as you type...'
   },
   plugins: [
     ["check-md", { pattern: "**/*.md" }],
