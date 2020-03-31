0a61866ed03d313b3975d83a575b841a44bc78dd
Clementine Urquizar <clementine@meilisearch.com>
2020-03-31 14:26:18 +0200

    Fix after linter

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -130,7 +130,8 @@ module.exports = {
     meilisearch: {
       hostUrl: "https://e10b17e6.getmeili.com",
       indexUid: "docs",
-      apiKey: "b4ff9313299e65ce4e259d3aa59730d2829c9a6805aa15b83cdbd63b20563155",
+      apiKey:
+        "b4ff9313299e65ce4e259d3aa59730d2829c9a6805aa15b83cdbd63b20563155",
     },
     searchPlaceholder: "Search as you type...",
   },
