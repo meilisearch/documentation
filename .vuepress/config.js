53c35bd89978aeecbc832f80be9f501ac5c93c22
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-07-02 11:16:35 +0200

    parent b3ec01913a59a9ef79b2f601b3fa3b35b1df3b7f author Charlotte Vermandel <charlottevermandel@gmail.com> 1593681395 +0200 committer Charlotte Vermandel <charlottevermandel@gmail.com> 1595338873 +0200
    Prepare Documentation for MeiliSearch v0.13
    
    Prepare Documentation for MeiliSearch v0.13
    
    Bump markdownlint-cli from 0.23.1 to 0.23.2
    
    Bumps [markdownlint-cli](https://github.com/igorshubovych/markdownlint-cli) from 0.23.1 to 0.23.2.
    - [Release notes](https://github.com/igorshubovych/markdownlint-cli/releases)
    - [Commits](https://github.com/igorshubovych/markdownlint-cli/compare/v0.23.1...v0.23.2)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Update resources page about SDKs/Integration
    
    update faceting code example
    
    Update gh-pages-scraping.yml
    
    Bump vuepress-plugin-img-lazy from 1.0.3 to 1.0.4
    
    Bumps [vuepress-plugin-img-lazy](https://github.com/tolking/vuepress-plugin-img-lazy) from 1.0.3 to 1.0.4.
    - [Release notes](https://github.com/tolking/vuepress-plugin-img-lazy/releases)
    - [Commits](https://github.com/tolking/vuepress-plugin-img-lazy/compare/v1.0.3...v1.0.4)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    documents placeholder search
    
    Update references/search.md
    
    requested changes
    
    Update references/search.md
    
    add post route documentation (#416)
    
    * Bump markdownlint-cli from 0.23.1 to 0.23.2
    
    Bumps [markdownlint-cli](https://github.com/igorshubovych/markdownlint-cli) from 0.23.1 to 0.23.2.
    - [Release notes](https://github.com/igorshubovych/markdownlint-cli/releases)
    - [Commits](https://github.com/igorshubovych/markdownlint-cli/compare/v0.23.1...v0.23.2)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Bump @popperjs/core from 2.4.2 to 2.4.4
    
    Bumps [@popperjs/core](https://github.com/popperjs/popper-core) from 2.4.2 to 2.4.4.
    - [Release notes](https://github.com/popperjs/popper-core/releases)
    - [Commits](https://github.com/popperjs/popper-core/compare/v2.4.2...v2.4.4)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Update resources page about SDKs/Integration
    
    * update faceting code example
    
    * Update gh-pages-scraping.yml
    
    * Bump vuepress-plugin-img-lazy from 1.0.3 to 1.0.4
    
    Bumps [vuepress-plugin-img-lazy](https://github.com/tolking/vuepress-plugin-img-lazy) from 1.0.3 to 1.0.4.
    - [Release notes](https://github.com/tolking/vuepress-plugin-img-lazy/releases)
    - [Commits](https://github.com/tolking/vuepress-plugin-img-lazy/compare/v1.0.3...v1.0.4)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * add post route documentation
    
    * add preflight request doc link
    
    * update search examples to use post
    
    * fix style
    
    * requested changed
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Co-authored-by: Clementine Urquizar <clementine@meilisearch.com>
    Co-authored-by: Rob Salmond <rob@salmond.ca>
    
    Documents update process (#461)
    
    * update faupdate faq.md
    
    * add update direction in install guide

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 const ogprefix = 'og: http://ogp.me/ns#'
 module.exports = {
-  title: 'MeiliSearch Documentation v0.12',
+  title: 'MeiliSearch Documentation v0.13',
   description: 'Open source Instant Search Engine',
   themeConfig: {
     repo: 'meilisearch/MeiliSearch',
