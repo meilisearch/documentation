4f4c21827c67ff059b1c2f4372eebdd93a00563e
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-08-31 16:47:19 +0200

    Update fanthom link

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -272,20 +272,12 @@ module.exports = {
     ],
     [
       'script',
-      {},
-      `
-    (function(f, a, t, h, o, m){
-      a[h]=a[h]||function(){
-        (a[h].q=a[h].q||[]).push(arguments)
-      };
-      o=f.createElement('script'),
-      m=f.getElementsByTagName('script')[0];
-      o.async=1; o.src=t; o.id='fathom-script';
-      m.parentNode.insertBefore(o,m)
-    })(document, window, '//analytics.meilisearch.com/tracker.js', 'fathom');
-    fathom('set', 'siteId', 'XQNHD');
-    fathom('trackPageview');
-    `,
+      {
+        src: 'https://cdn.usefathom.com/script.js',
+        spa: 'auto',
+        site: 'WIMXMDAA',
+        defer: true,
+      },
     ],
   ],
 }
