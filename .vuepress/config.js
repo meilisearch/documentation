c461fbe7edf3e964bd033cb50a0727404480c8f8
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-09-02 17:45:17 +0200

    Remove comment in config.hs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 const ogprefix = 'og: http://ogp.me/ns#'
 module.exports = {
-  title: 'MeiliSearch Documentation v0.14',
+  title: 'MeiliSearch Documentation v0.13',
   description: 'Open source Instant Search Engine',
   themeConfig: {
     repo: 'meilisearch/MeiliSearch',
