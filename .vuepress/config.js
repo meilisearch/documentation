56a38ccbc19f3502d635775285fe4fab7ad0ca8a
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-09-03 12:36:19 +0200

    Safeguards guide

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -57,6 +57,7 @@ module.exports = {
             '/guides/advanced_guides/synonyms',
             '/guides/advanced_guides/stop_words',
             '/guides/advanced_guides/distinct',
+            '/guides/advanced_guides/safeguards',
             '/guides/advanced_guides/field_properties',
             '/guides/advanced_guides/typotolerance',
             '/guides/advanced_guides/prefix',
