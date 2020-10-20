c624f8d4a455d46e0a0e37aad36490925092b092
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-10-20 11:06:30 +0200

    Prepare documentation for release 11/2020 of MeiliSearch

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 const ogprefix = 'og: http://ogp.me/ns#'
 module.exports = {
-  title: 'MeiliSearch Documentation v0.15',
+  title: 'MeiliSearch Documentation v0.16',
   description: 'Open source Instant Search Engine',
   themeConfig: {
     repo: 'meilisearch/MeiliSearch',
