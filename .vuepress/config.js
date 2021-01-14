4ec65600fd78df0ae469cc106e1124fb6c6a6257
cvermand <33010418+bidoubiwa@users.noreply.github.com>
2021-01-14 18:17:19 +0100

    Update .vuepress/config.js
    Co-authored-by: Tommy <68053732+react-learner@users.noreply.github.com>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 const ogprefix = 'og: http://ogp.me/ns#'
 module.exports = {
-  title: 'MeiliSearch Documentation v0.16',
+  title: 'MeiliSearch Documentation v0.18',
   description: 'Open source Instant Search Engine',
   themeConfig: {
     repo: 'meilisearch/MeiliSearch',
