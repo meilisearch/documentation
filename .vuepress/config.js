d095c0018e51b90d01365265c762895d46d50a68
gui machiavelli <hey@guimachiavelli.com>
2021-09-13 17:01:30 +0200

    version bump to v0.23

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 const ogprefix = 'og: http://ogp.me/ns#'
 module.exports = {
-  title: 'MeiliSearch Documentation v0.22',
+  title: 'MeiliSearch Documentation v0.23',
   description: 'Open source Instant Search Engine',
   themeConfig: {
     repo: 'meilisearch/MeiliSearch',
