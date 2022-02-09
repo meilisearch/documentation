6246af5dde68bce1f7b83d141d2254ae13165405
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-02-09 14:00:59 +0100

    Improve error message

--- .vuepress/config-path-checker/find-and-check.js
+++ .vuepress/config-path-checker/find-and-check.js
@@ -2,12 +2,13 @@ const { isObject } = require('./utils')
 const { addError } = require('./result-report')
 const path = require('path')
 
-function checkPath(givenPath) {
+function checkPath(givenPath, elem) {
   if (givenPath.slice(-1) !== '/') {
+    const origin = (elem && elem.title) ? `section ${elem.title}` : 'config.json'
     addError.call(this, {
       type: 'error',
-      errMsg: 'Path in config.json file is missing a trailing slash',
-      fileUrl: path.join(__dirname, 'find-and-check.js'),
+      errMsg: `In ${origin} the path is missing a trailing slash`,
+      fileUrl: path.join(process.cwd(), '/.vuepress/config.json'),
       fullText: `path: '${givenPath}'`,
       path: givenPath,
     })
@@ -17,7 +18,7 @@ function checkPath(givenPath) {
 function searchForPath(element) {
   if (isObject(element)) {
     if (element.path && element.children) {
-      checkPath.call(this, element.path)
+      checkPath.call(this, element.path, element)
     }
     if (element.children) {
       SearchForChilds.call(this, element.children)
