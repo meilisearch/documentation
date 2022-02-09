7ec9d3899f6b4fbd50553ef6b57e4e5fffa6e796
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-02-09 16:45:14 +0100

    Add Poppins font manually

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -317,6 +317,9 @@ module.exports = {
   ],
   head: [
     ['link', { rel: 'icon', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap' }],
     ['meta', { charset: 'utf-8' }],
     [
       'meta',
