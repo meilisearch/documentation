334c9ccf717fe81ba4698570091eb19635e2f980
gui machiavelli <hey@guimachiavelli.com>
2022-02-23 16:26:00 +0100

    tenant tokens: fix linting errors

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -126,7 +126,8 @@ module.exports = {
             },
           ],
         },
-        { title: '🔐 Security and permissions',
+        {
+          title: '🔐 Security and permissions',
           collapsable: false,
           path: '/learn/security/master_api_keys/',
           children: [
