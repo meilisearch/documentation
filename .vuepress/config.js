fee2f49954a1134c434fa9ede89cbb5647a06590
Tommy Melvin <tommy@meilisearch.com>
2022-03-10 14:50:08 +0100

    Fix style

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -232,9 +232,9 @@ module.exports = {
             },
             {
               title: 'Auto-batching',
-              path: '/learn/experimental/auto-batching'
+              path: '/learn/experimental/auto-batching',
             },
-          ]
+          ],
         },
         {
           title: '👐 Contributing',
