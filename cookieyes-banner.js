c340d17de894f6e1625fce73fd1807fa99d7feaf
gui machiavelli <hey@guimachiavelli.com>
2025-04-29 17:24:32 +0200

    add cookie banner script

---
+++ cookieyes-banner.js
@@ -0,0 +1,13 @@
+const cookieyesSrc = "https://cdn-cookieyes.com/client_data/0ec5a8e516eccaa724a461f6/script.js";
+const bannerId = "cookieyes";
+
+function createBanner() {
+  const el = document.createElement("script");
+  el.src = cookieyesSrc;
+  el.id = bannerId;
+  el.type = "text/javascript"
+
+  document.head.appendChild(el);
+}
+
+createBanner();
