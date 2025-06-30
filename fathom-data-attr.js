8c5aaf68670ae33356421fd94f918290175b38d3
gui machiavelli <gui@meilisearch.com>
2025-06-30 17:28:52 +0200

    Patch fathom analytics (#3292)
    * try to patch fathom's script el to support SPAs
    
    * Update code samples [skip ci]
    
    * add timeout, ensure multiple attempts at adding, limit var scope

---
+++ fathom-data-attr.js
@@ -0,0 +1,22 @@
+(function(){
+  let attempts = 0;
+
+  function addDataAttr() {
+    const spaValue = "auto";
+    const el = document.getElementById("fathom-script");
+    attempts += 1;
+    
+    if (!el && attempts < 10) {
+      window.setTimeout(addDataAttr, 100);
+      return;
+    }
+
+    const dataSpaValue = el.dataset.spa;
+
+    if (dataSpaValue === undefined) {
+      el.dataset.spa = spaValue;
+    }
+  }
+
+  addDataAttr();
+})();
