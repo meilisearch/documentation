41975b188d3f81a1f853ba8d317234f46d74231a
gui machiavelli <gui@meilisearch.com>
2025-09-02 15:13:57 +0200

    Add GTM script as requested by analytics consultant (#3350)

---
+++ gtm.js
@@ -0,0 +1,13 @@
+(function(){
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'ad_user_data': 'denied',
+    'ad_personalization': 'denied',
+    'analytics_storage': 'denied'
+  });
+
+})();
